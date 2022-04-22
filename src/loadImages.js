0cec50e3a42b6c84c8b80a855feacc9a4c7fcdc4
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-04-22 21:41:51 +0100

    UI view updated

--- src/loadImages.js
+++ src/loadImages.js
@@ -1,9 +1,11 @@
 import todoIcon from "./icons/logo.png";
 import deleteIcon from "./icons/delete.png";
 import editIcon from "./icons/edit.png";
+import githubIcon from "./icons/github.png";
 
 const loadImages = function () {
   const logoIcon = document.querySelector(".logo-icon");
+  const githubImg = document.querySelector(".github-icon");
   const allDeleteImgs = document.querySelectorAll(".delete-icon");
   const allEditImgs = document.querySelectorAll(".edit-icon");
   allDeleteImgs.forEach(function (icon) {
@@ -13,6 +15,7 @@ const loadImages = function () {
     icon.src = editIcon;
   });
   logoIcon.setAttribute("src", todoIcon);
+  githubImg.setAttribute("src", githubIcon);
 };
 
 export default loadImages;
